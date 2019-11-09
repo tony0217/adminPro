f7dfc18df02806f7a62c84d4388c11fdfdc720c3
tony_217 <www.anthonyhenriquez@gmail.com>
2019-11-09 00:50:15 -0500

    start project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './login/register.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { PagesComponent } from './pages/pages.component';
+
+
+
+const routes: Routes = [
+  // ruta principal
+  {path:'', 
+  component: PagesComponent,
+  // hijos del pages 
+  children:[
+    {path:'dashboard', component: DashboardComponent},
+    {path:'progress', component: ProgressComponent},
+    {path:'grafica1', component: Graficas1Component},
+    {path:'',redirectTo:'/dashboard' , pathMatch: 'full'},
+  ]
+
+},
+
+  {path:'login', component: LoginComponent},
+  {path:'register', component: RegisterComponent},
+  {path:'**', component: NopagefoundComponent},
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
